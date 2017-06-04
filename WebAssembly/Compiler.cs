4d5a765d17854345eee23af5db8d3e5d8507adea
Ryan Lamansky <kardax@kardax.com>
2017-06-04 12:34:03 -0500

    CIL generation management moved from Parsed to Compiler.

--- WebAssembly/Compiler.cs
+++ WebAssembly/Compiler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 
 namespace WebAssembly
 {
@@ -71,9 +74,168 @@ namespace WebAssembly
 				}
 			}
 
-			var constructor = parsed.Compile(typeof(Instance<TExports>), typeof(TExports));
+			var constructor = FromParsed(parsed, typeof(Instance<TExports>), typeof(TExports));
 
 			return () => (Instance<TExports>)constructor.Invoke(null);
+		}
+
+		private static ConstructorInfo FromParsed(Parsed parsed, System.Type instanceContainer, System.Type exportContainer)
+		{
+			var module = AssemblyBuilder.DefineDynamicAssembly(
+				new AssemblyName("CompiledWebAssembly"),
+				AssemblyBuilderAccess.RunAndCollect
+				)
+				.DefineDynamicModule("CompiledWebAssembly")
+				;
+
+			const TypeAttributes classAttributes =
+				TypeAttributes.Public |
+				TypeAttributes.Class |
+				TypeAttributes.BeforeFieldInit
+				;
+
+			const MethodAttributes constructorAttributes =
+				MethodAttributes.Public |
+				MethodAttributes.HideBySig |
+				MethodAttributes.SpecialName |
+				MethodAttributes.RTSpecialName
+				;
+
+			const MethodAttributes exportedFunctionAttributes =
+				MethodAttributes.Public |
+				MethodAttributes.Virtual |
+				MethodAttributes.Final |
+				MethodAttributes.HideBySig
+				;
+
+			TypeInfo exports;
+			var exportsBuilder = module.DefineType("CompiledExports", classAttributes, exportContainer);
+			var linearMemoryStart = exportsBuilder.DefineField("☣ Linear Memory Start", typeof(void*), FieldAttributes.Private);
+			var linearMemorySize = exportsBuilder.DefineField("☣ Linear Memory Size", typeof(uint), FieldAttributes.Private);
+
+			{
+				var instanceConstructor = exportsBuilder.DefineConstructor(constructorAttributes, CallingConventions.Standard, new System.Type[] {
+					typeof(IntPtr),
+					typeof(uint),
+				});
+				var il = instanceConstructor.GetILGenerator();
+				{
+					var usableConstructor = exportContainer.GetTypeInfo().DeclaredConstructors.FirstOrDefault(c => c.GetParameters().Length == 0);
+					if (usableConstructor != null)
+					{
+						il.Emit(OpCodes.Ldarg_0);
+						il.Emit(OpCodes.Call, usableConstructor);
+					}
+
+					il.Emit(OpCodes.Ldarg_0);
+					il.Emit(OpCodes.Ldarg_1);
+					il.Emit(OpCodes.Stfld, linearMemoryStart);
+
+					il.Emit(OpCodes.Ldarg_0);
+					il.Emit(OpCodes.Ldarg_2);
+					il.Emit(OpCodes.Stfld, linearMemorySize);
+				}
+				il.Emit(OpCodes.Ret);
+
+				var exportedFunctions = parsed.ExportedFunctions;
+
+				if (exportedFunctions != null)
+				{
+					var context = new CompilationContext(exportsBuilder, linearMemoryStart, linearMemorySize);
+
+					for (var i = 0; i < exportedFunctions.Length; i++)
+					{
+						var exported = exportedFunctions[i];
+						var func = parsed.Functions[exported.Value];
+
+						var method = exportsBuilder.DefineMethod(
+							exported.Key,
+							exportedFunctionAttributes,
+							CallingConventions.HasThis,
+							func.Signature.ReturnTypes.FirstOrDefault(),
+							func.Signature.ParameterTypes
+							);
+
+						il = method.GetILGenerator();
+
+						context.Reset(
+							il,
+							func,
+							func.Signature.RawParameterTypes.Concat(
+								func
+								.Locals
+								.SelectMany(locals => Enumerable.Range(0, checked((int)locals.Count)).Select(_ => locals.Type))
+								).ToArray()
+							);
+						var instructions = func.Instructions;
+						for (var j = 0; j < instructions.Length; j++)
+						{
+							var instruction = instructions[j];
+							instruction.Compile(context);
+							context.Previous = instruction.OpCode;
+						}
+					}
+				}
+			}
+
+			exports = exportsBuilder.CreateTypeInfo();
+
+			TypeInfo instance;
+			{
+				var instanceBuilder = module.DefineType("CompiledInstance", classAttributes, instanceContainer);
+				var instanceConstructor = instanceBuilder.DefineConstructor(constructorAttributes, CallingConventions.Standard, null);
+				var il = instanceConstructor.GetILGenerator();
+				var memoryAllocated = checked(parsed.MemoryPagesMaximum * Memory.PageSize);
+
+				if (memoryAllocated > 0)
+				{
+					var start = il.DeclareLocal(typeof(IntPtr));
+					il.Emit(OpCodes.Ldc_I4, memoryAllocated);
+					il.Emit(OpCodes.Call, typeof(Marshal)
+						.GetTypeInfo()
+						.GetDeclaredMethods(nameof(Marshal.AllocHGlobal))
+						.First(m =>
+						{
+							var parms = m.GetParameters();
+							return parms.Length == 1 && parms[0].ParameterType == typeof(int);
+						})
+						);
+					il.Emit(OpCodes.Stloc_0);
+
+					il.Emit(OpCodes.Ldarg_0);
+					il.Emit(OpCodes.Ldloc_0);
+					il.Emit(OpCodes.Ldc_I4, memoryAllocated);
+					il.Emit(OpCodes.Newobj, exports.DeclaredConstructors.First());
+
+					il.Emit(OpCodes.Ldloc_0);
+					il.Emit(OpCodes.Ldloc_0);
+					il.Emit(OpCodes.Ldc_I4, memoryAllocated);
+					il.Emit(OpCodes.Add);
+				}
+				else
+				{
+					il.Emit(OpCodes.Ldarg_0);
+					il.Emit(OpCodes.Ldc_I4_0);
+					il.Emit(OpCodes.Ldc_I4_0);
+					il.Emit(OpCodes.Newobj, exports.DeclaredConstructors.First());
+
+					il.Emit(OpCodes.Ldc_I4_0);
+					il.Emit(OpCodes.Ldc_I4_0);
+				}
+				il.Emit(OpCodes.Call, instanceContainer
+					.GetTypeInfo()
+					.DeclaredConstructors
+					.First(info => info.GetParameters()
+					.FirstOrDefault()
+					?.ParameterType == exportContainer
+					)
+					);
+				il.Emit(OpCodes.Ret);
+
+				instance = instanceBuilder.CreateTypeInfo();
+			}
+
+			return instance.DeclaredConstructors.First();
 		}
 	}
 }
