73d8f7c91e018ef41f904776d4e8013927fc0a71
Ryan Lamansky <kardax@kardax.com>
2019-07-27 17:42:15 -0500

    Official tests f32.wast and f64.wast integrated after converting to WASM/JSON with WABT's wast2json tool.

---
+++ WebAssembly.Tests/Runtime/SpecTestRunner.cs
@@ -0,0 +1,265 @@
+﻿using JsonSubTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WebAssembly.Runtime
+{
+    static class SpecTestRunner
+    {
+        public static void Run<TExports>(string pathBase, string json)
+            where TExports : class
+        {
+            TestInfo testInfo;
+            using (var reader = new StreamReader(Path.Combine(pathBase, json)))
+            {
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(JsonSubtypesConverterBuilder
+                    .Of(typeof(Command), "type")
+                    .RegisterSubtype(typeof(ModuleCommand), CommandType.module)
+                    .RegisterSubtype(typeof(AssertReturn), CommandType.assert_return)
+                    .RegisterSubtype(typeof(AssertReturnCanonicalNan), CommandType.assert_return_canonical_nan)
+                    .RegisterSubtype(typeof(AssertReturnArithmeticNan), CommandType.assert_return_arithmetic_nan)
+                    .RegisterSubtype(typeof(AssertInvalid), CommandType.assert_invalid)
+                    .SerializeDiscriminatorProperty()
+                    .Build());
+                settings.Converters.Add(JsonSubtypesConverterBuilder
+                    .Of(typeof(TestAction), "type")
+                    .RegisterSubtype(typeof(Invoke), TestActionType.invoke)
+                    .SerializeDiscriminatorProperty()
+                    .Build());
+                settings.Converters.Add(JsonSubtypesConverterBuilder
+                    .Of(typeof(TypedValue), "type")
+                    .RegisterSubtype(typeof(Int32Value), RawValueType.i32)
+                    .RegisterSubtype(typeof(Int64Value), RawValueType.i64)
+                    .RegisterSubtype(typeof(Float32Value), RawValueType.f32)
+                    .RegisterSubtype(typeof(Float64Value), RawValueType.f64)
+                    .SerializeDiscriminatorProperty()
+                    .Build());
+                testInfo = (TestInfo)JsonSerializer.Create(settings).Deserialize(reader, typeof(TestInfo));
+            }
+
+            Instance<TExports> instance = null;
+            TExports exports = null;
+            Dictionary<string, MethodInfo> methodsByName = null;
+
+            foreach (var command in testInfo.commands)
+            {
+                switch (command)
+                {
+                    case ModuleCommand module:
+                        instance = Compile.FromBinary<TExports>(Path.Combine(pathBase, module.filename))(new ImportDictionary());
+                        exports = instance.Exports;
+                        methodsByName = exports.GetType().GetMethods().ToDictionary(m => m.Name);
+                        continue;
+                    case AssertReturn assert:
+                        switch (assert.action)
+                        {
+                            case Invoke invoke:
+                                Assert.IsNotNull(methodsByName);
+                                Assert.IsTrue(methodsByName.TryGetValue(invoke.field, out var methodInfo));
+                                var result = methodInfo.Invoke(exports, invoke.args.Select(arg => arg.BoxedValue).ToArray());
+                                Assert.AreEqual(assert.expected[0].BoxedValue, result);
+                                continue;
+                            default:
+                                throw new AssertFailedException($"{assert.action} doesn't have a test procedure set up.");
+                        }
+                    case AssertReturnCanonicalNan assert:
+                        switch (assert.action)
+                        {
+                            case Invoke invoke:
+                                Assert.IsNotNull(methodsByName);
+                                Assert.IsTrue(methodsByName.TryGetValue(invoke.field, out var methodInfo));
+                                var result = methodInfo.Invoke(exports, invoke.args.Select(arg => arg.BoxedValue).ToArray());
+                                switch (assert.expected[0].type)
+                                {
+                                    case RawValueType.f32:
+                                        Assert.IsTrue(float.IsNaN((float)result));
+                                        continue;
+                                    case RawValueType.f64:
+                                        Assert.IsTrue(double.IsNaN((double)result));
+                                        continue;
+                                    default:
+                                        throw new AssertFailedException($"{assert.expected[0].type} doesn't support NaN checks.");
+                                }
+                            default:
+                                throw new AssertFailedException($"{assert.action} doesn't have a test procedure set up.");
+                        }
+                    case AssertReturnArithmeticNan assert:
+                        switch (assert.action)
+                        {
+                            case Invoke invoke:
+                                Assert.IsNotNull(methodsByName);
+                                Assert.IsTrue(methodsByName.TryGetValue(invoke.field, out var methodInfo));
+                                var result = methodInfo.Invoke(exports, invoke.args.Select(arg => arg.BoxedValue).ToArray());
+                                switch (assert.expected[0].type)
+                                {
+                                    case RawValueType.f32:
+                                        Assert.IsTrue(float.IsNaN((float)result));
+                                        continue;
+                                    case RawValueType.f64:
+                                        Assert.IsTrue(double.IsNaN((double)result));
+                                        continue;
+                                    default:
+                                        throw new AssertFailedException($"{assert.expected[0].type} doesn't support NaN checks.");
+                                }
+                            default:
+                                throw new AssertFailedException($"{assert.action} doesn't have a test procedure set up.");
+                        }
+                    case AssertInvalid assert:
+                        Assert.ThrowsException<StackTypeInvalidException>(() => Compile.FromBinary<TExports>(Path.Combine(pathBase, assert.filename)));
+                        continue;
+                    default:
+                        throw new AssertFailedException($"{command} doesn't have a test procedure set up.");
+                }
+            }
+        }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        enum CommandType
+        {
+            module,
+            assert_return,
+            assert_return_canonical_nan,
+            assert_return_arithmetic_nan,
+            assert_invalid,
+        }
+
+#pragma warning disable 649
+        class TestInfo
+        {
+            public string source_filename;
+            public Command[] commands;
+        }
+
+        abstract class Command
+        {
+            public CommandType type;
+            public uint line;
+
+            public override string ToString() => type.ToString();
+        }
+
+        class ModuleCommand : Command
+        {
+            public string filename;
+
+            public override string ToString() => $"{base.ToString()}: {filename}";
+        }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        enum RawValueType
+        {
+            i32 = WebAssemblyValueType.Int32,
+            i64 = WebAssemblyValueType.Int64,
+            f32 = WebAssemblyValueType.Float32,
+            f64 = WebAssemblyValueType.Float64,
+        }
+
+        class TypeOnly
+        {
+            public RawValueType type;
+
+            public override string ToString() => type.ToString();
+        }
+
+        abstract class TypedValue : TypeOnly
+        {
+            public abstract object BoxedValue { get; }
+        }
+
+        class Int32Value : TypedValue
+        {
+            public uint value;
+
+            public override object BoxedValue => value;
+
+            public override string ToString() => $"{type}: {value}";
+        }
+
+        class Int64Value : TypedValue
+        {
+            public ulong value;
+
+            public override object BoxedValue => value;
+
+            public override string ToString() => $"{type}: {value}";
+        }
+
+        class Float32Value : Int32Value
+        {
+            public override object BoxedValue => BitConverter.Int32BitsToSingle(unchecked((int)value));
+
+            public override string ToString() => $"{type}: {BoxedValue}";
+        }
+
+        class Float64Value : Int64Value
+        {
+            public override object BoxedValue => BitConverter.Int64BitsToDouble(unchecked((long)value));
+
+            public override string ToString() => $"{type}: {BoxedValue}";
+        }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        enum TestActionType
+        {
+            invoke,
+        }
+
+        abstract class TestAction
+        {
+            public TestActionType type;
+        }
+
+        class Invoke : TestAction
+        {
+            public string field;
+            public TypedValue[] args;
+
+            public override string ToString() => $"{field} [{string.Join(',', (IEnumerable<TypedValue>)args)}]";
+        }
+
+        abstract class AssertCommand : Command
+        {
+            public TestAction action;
+
+            public override string ToString() => $"{base.ToString()}: {action.ToString()}";
+        }
+
+        class AssertReturn : AssertCommand
+        {
+            public TypedValue[] expected;
+
+            public override string ToString() => $"{base.ToString()} = [{string.Join(',', (IEnumerable<TypedValue>)expected)}]";
+        }
+
+        class AssertReturnCanonicalNan : AssertCommand
+        {
+            public TypeOnly[] expected;
+
+            public override string ToString() => $"{base.ToString()} = [{string.Join(',', (IEnumerable<TypeOnly>)expected)}]";
+        }
+
+        class AssertReturnArithmeticNan : AssertCommand
+        {
+            public TypeOnly[] expected;
+
+            public override string ToString() => $"{base.ToString()} = [{string.Join(',', (IEnumerable<TypeOnly>)expected)}]";
+        }
+
+        class AssertInvalid : Command
+        {
+            public string filename;
+            public string text;
+            public string module_type;
+
+            public override string ToString() => $"{base.ToString()}: {filename} \"{text}\" {module_type}";
+        }
+#pragma warning restore
+    }
+}
---
+++ WebAssembly.Tests/Runtime/SpecTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace WebAssembly.Runtime
+{
+    /// <summary>
+    /// Runs the official specification's tests.
+    /// </summary>
+    [TestClass]
+    public class SpecTests
+    {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public abstract class FloatingPointMath<T>
+        {
+            public abstract T add(T x, T y);
+            public abstract T sub(T x, T y);
+            public abstract T mul(T x, T y);
+            public abstract T div(T x, T y);
+            public abstract T sqrt(T x);
+            public abstract T min(T x, T y);
+            public abstract T max(T x, T y);
+            public abstract T ceil(T x);
+            public abstract T floor(T x);
+            public abstract T trunc(T x);
+            public abstract T nearest(T x);
+        }
+#pragma warning restore
+
+        /// <summary>
+        /// Runs the f32 tests.
+        /// </summary>
+        [TestMethod]
+        public void SpecTest_f32()
+        {
+            SpecTestRunner.Run<FloatingPointMath<float>>(Path.Combine("Runtime", "SpecTestData", "f32"), "f32.json");
+        }
+
+        /// <summary>
+        /// Runs the f64 tests.
+        /// </summary>
+        [TestMethod]
+        public void SpecTest_f64()
+        {
+            SpecTestRunner.Run<FloatingPointMath<double>>(Path.Combine("Runtime", "SpecTestData", "f64"), "f64.json");
+        }
+    }
+}
