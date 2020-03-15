6ef67183187b5f399afd970ac5c437d529c8f8c0
Ryan Lamansky <kardax@kardax.com>
2020-03-14 22:33:10 -0500

    Runtime global imports now accept raw delegates.

--- WebAssembly/Runtime/GlobalImport.cs
+++ WebAssembly/Runtime/GlobalImport.cs
@@ -31,7 +31,7 @@ namespace WebAssembly.Runtime
         /// <param name="setter">The method to use for write requests, or null (the default).</param>
         /// <exception cref="ArgumentNullException"><paramref name="getter"/> cannot be null.</exception>
         public GlobalImport(Func<int> getter, Action<int>? setter = null)
-            : this( getter, setter, WebAssemblyValueType.Int32)
+            : this(getter, setter, WebAssemblyValueType.Int32)
         {
         }
 
@@ -92,9 +92,57 @@ namespace WebAssembly.Runtime
 
         private GlobalImport(Delegate getter, Delegate? setter, WebAssemblyValueType type)
         {
+            if (getter == null)
+                throw new ArgumentNullException(nameof(getter));
+
             this.Getter = getter;
             this.Setter = setter;
             this.GetterType = type;
+        }
+
+        static WebAssemblyValueType DetermineValueType(Delegate getter, Delegate? setter)
+        {
+            if (getter == null)
+                throw new ArgumentNullException(nameof(getter));
+
+            if (getter.Method.GetParameters().Length != 0)
+                throw new ArgumentException("getter cannot have parameters.", nameof(getter));
+
+            var gret = getter.Method.ReturnType;
+
+            if (gret == null || gret.TryConvertToValueType(out var gtype))
+                throw new ArgumentException("getter does not return a compatible type.", nameof(getter));
+
+            if (setter != null)
+            {
+                if (setter.Method.ReturnType != null)
+                    throw new ArgumentException("setter cannot have a return type.", nameof(setter));
+
+                var sparms = setter.Method.GetParameters();
+                if (sparms.Length != 1)
+                    throw new ArgumentException("setter must have exactly 1 parameter.", nameof(setter));
+
+                var sparm = sparms[0].ParameterType;
+                if (sparm == null || sparm.TryConvertToValueType(out var stype))
+                    throw new ArgumentException("setter does not accept a compatible type.", nameof(setter));
+
+                if (stype != gtype)
+                    throw new ArgumentException("setter does not accept the same type as getter returns.", nameof(setter));
+            }
+
+            return gtype;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="GlobalImport"/> instance with the provided delegates.
+        /// </summary>
+        /// <param name="getter">The method to use for read requests.</param>
+        /// <param name="setter">The method to use for write requests, or null (the default).</param>
+        /// <exception cref="ArgumentNullException"><paramref name="getter"/> cannot be null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="getter"/> and/or <paramref name="setter"/> are incompatible.</exception>
+        public GlobalImport(Delegate getter, Delegate? setter = null)
+            : this(getter, setter, DetermineValueType(getter, setter))
+        {
         }
     }
 }
