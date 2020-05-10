5768f2e575f94eaff419b131e8a486a47825014e
Ryan Lamansky <kardax@kardax.com>
2020-05-10 09:26:21 -0500

    Disabled parallel testing for Release builds to prevent CI from triggering what appears to be a bug in MSTest.

--- WebAssembly.Tests/Properties/AssemblyInfo.cs
+++ WebAssembly.Tests/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+// An apparent bug in MSTest causes occasional random test failures when parallelism is enabled.
+#if DEBUG // Only enable parallelism for debug builds to prevent it from stopping CI builds.
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+#endif
