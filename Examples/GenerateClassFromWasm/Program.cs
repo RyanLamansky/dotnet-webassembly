2cdb75f1dfe449f0f508ac21eda159ac539e0940
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2021-05-02 19:11:22 -0500

    Added a partial example for generating an abstract class for the WebAssembly compiler to implement.

---
+++ Examples/GenerateClassFromWasm/Program.cs
@@ -0,0 +1,72 @@
+﻿// This sample isn't complete but may be useful as a starting point for handling exports.
+// The WebAssembly.Tests project has cases for all export types, so you can track down how to use them there until this example is finished.
+using System;
+using System.Linq;
+using WebAssembly; // Acquire from https://www.nuget.org/packages/WebAssembly
+
+// Create a WASM with a variety of imports/exports for illustrative purposes.
+// When working with an existing WASM, you'd use `Module.ReadFromBinary`.
+var module = Sample.GenerateExample();
+
+const string indent = "    ";
+Console.WriteLine("abstract class Exports");
+Console.WriteLine("{");
+Console.WriteLine($"{indent}protected Exports()");
+Console.WriteLine($"{indent}{{");
+Console.WriteLine($"{indent}}}");
+Console.WriteLine();
+
+foreach (var export in module.Exports)
+{
+    static string WebAssemblyValueTypeToNative(WebAssemblyValueType type) => type switch
+    {
+        WebAssemblyValueType.Int32 => "int",
+        WebAssemblyValueType.Int64 => "long",
+        WebAssemblyValueType.Float32 => "float",
+        WebAssemblyValueType.Float64 => "double",
+        _ => throw new NotSupportedException(),
+    };
+
+    switch (export.Kind)
+    {
+        case ExternalKind.Function:
+            // TODO: Account for the indexes of imported functions--first internal function's index is 1 after the last imported.
+            var signature = module.Types[(int)module.Functions[(int)export.Index].Type];
+            Console.Write(indent);
+            Console.Write("public abstract ");
+            Console.Write(signature.Returns.Count == 0 ? "void" : WebAssemblyValueTypeToNative(signature.Returns[0]));
+            Console.Write(' ');
+            Console.Write(export.Name);
+            Console.Write('(');
+            Console.Write(string.Join(", ", signature.Parameters.Select((type, index) => $"{WebAssemblyValueTypeToNative(type)} p{index}")));
+            Console.WriteLine(");");
+            continue;
+
+        case ExternalKind.Table:
+            // TODO
+            continue;
+
+        case ExternalKind.Memory:
+            // TODO
+            continue;
+
+        case ExternalKind.Global:
+            // TODO: Account for the indexes of imported globals--first internal global's index is 1 after the last imported.
+            var global = module.Globals[(int)export.Index];
+            Console.Write(indent);
+            Console.Write("public abstract ");
+            Console.Write(WebAssemblyValueTypeToNative(global.ContentType));
+            Console.Write(' ');
+            Console.Write(export.Name);
+            Console.Write(" { get;");
+            if (global.IsMutable)
+                Console.Write(" set;");
+            Console.WriteLine(" }");
+            continue;
+    }
+}
+
+Console.WriteLine("}");
+
+// After generating the code, it can be used as the type parameter for `Compile.FromBinary`.
+// The compiler will inherit and implement the class.
---
+++ Examples/GenerateClassFromWasm/Sample.cs
@@ -0,0 +1,55 @@
+﻿using WebAssembly; // Acquire from https://www.nuget.org/packages/WebAssembly
+using WebAssembly.Instructions;
+
+static class Sample
+{
+    public static Module GenerateExample() => new()
+    {
+        Types = new WebAssemblyType[]
+        {
+            new WebAssemblyType
+            {
+                Returns = new WebAssemblyValueType[] { WebAssemblyValueType.Int32 },
+                Parameters = new WebAssemblyValueType[]
+                {
+                    WebAssemblyValueType.Int32,
+                    WebAssemblyValueType.Int64,
+                    WebAssemblyValueType.Float32,
+                    WebAssemblyValueType.Float64,
+                },
+            },
+            new WebAssemblyType
+            {
+            },
+        },
+        Functions = new Function[]
+        {
+            new Function(0),
+            new Function(1),
+        },
+        Codes = new FunctionBody[]
+        {
+            new FunctionBody(new LocalGet(), new End()),
+            new FunctionBody(new End()),
+        },
+        Globals = new Global[]
+        {
+            new Global
+            {
+                ContentType = WebAssemblyValueType.Int32,
+            },
+            new Global
+            {
+                ContentType = WebAssemblyValueType.Int32,
+                IsMutable = true,
+            }
+        },
+        Exports = new Export[]
+        {
+            new Export("Variety", 0, ExternalKind.Function),
+            new Export("DoNothing", 1, ExternalKind.Function),
+            new Export("ReadOnlyInt32Global", 0, ExternalKind.Global),
+            new Export("ReadWriteInt32Global", 1, ExternalKind.Global),
+        },
+    };
+}
